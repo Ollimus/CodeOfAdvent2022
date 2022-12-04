f9fd7619c6e002bcc9260648013e7b701b53fea6
Olli <Olli.Hirvonen@hotmail.fi>
2022-12-04 02:43:31 +0200

    Brainstorming using enums.

--- src/Day2/Program.cs
+++ src/Day2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*
+ * A = Rock
+ * B = Paper
+ * C = Scissors
+ * 
+ * X = Rock
+ * Y = Paper
+ * Z = Scissors
+*/
+
+#region part1
+#endregion
+
+#region part2
+
+#endregion
+
+enum RockPaperScissor
+{
+    Rock = 'A',
+    Paper = 'B',
+    Scissor = 'C'
+};
+
+enum EncodedRockPaperScissor
+{
+    Rock = 'X',
+    Paper = 'Y',
+    Scissor = 'Z'
+};
